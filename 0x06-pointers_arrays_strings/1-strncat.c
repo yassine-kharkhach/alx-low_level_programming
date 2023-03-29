ba6da5131fad0f27c6fa4fcb5262c1c979b56d3a
Your Name <you@example.com>
2023-03-29 14:11:27 +0000

    pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src){
+        int i = 0;
+        int j = 0;
+
+        while (dest[i])
+        {
+            dest[i] == dest[i];
+                i++;
+        }
+        while (src[j])
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+        return(dest);
+}i
